c00b4020957c40aa9a5a9db50aa761607dfa085c
asr <asr12211@outlook.com>
2023-05-27 12:10:10 +0300

    search page done

--- client/src/pages/search/search.jsx
+++ client/src/pages/search/search.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./search.css"
+import "./search.css";
 import Items from "../../components/items/items";
 
 // TODO: better styling and responsive
@@ -7,24 +7,33 @@ import Items from "../../components/items/items";
 function Search() {
 
    const [searchTerm, setSearchTerm] = useState("");
-   // const itemsRef = useRef();
 
-   return <div className="main-padding">
-      <div className="search-img-container">
+   function handleFocus() {
+      const DEVICEWIDTH = document.documentElement.clientWidth;
+      document.querySelector(".search-background").style.opacity = 0;
+      document.querySelector(".search-img-container").style.top = DEVICEWIDTH >= 550 ? "8vh" : "3vh";
+      document.querySelector(".search-img-container").style.padding = "15px";
+      document.querySelector(".search-input-container").style.top = DEVICEWIDTH >= 550 ? "25vh" : "17vh";
+   }
+
+   return <div className="search-container">
+      <img src="images/home/black.png" alt="search background" className="search-background img transition-05"/>
+      <div className="search-img-container transition-05">
          <img src="/icons/asr-logo.svg" alt="ASR Logo" className="img"/>
       </div>
-      <div className="grid search-input-container">
+      <div className="search-input-container transition-05">
          <input
             type="search"
             placeholder="search..."
             className="search-input"
             onKeyDown={e => {
-               if (e.key === "Enter") 
-                  setSearchTerm(e.target.value)
+               if (e.key === "Enter")
+                  setSearchTerm(e.target.value);
             }}
+            onFocus={handleFocus}
          />
       </div>
-      <section >
+      <section className="search-items-container">
          {searchTerm && <Items
             endpoint={`/items/search?term=${searchTerm}&page=`}
             queryId={searchTerm}
