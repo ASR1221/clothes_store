caa7c55fc192647aefc1f71c80764ad99c13e0de
asr <asr12211@outlook.com>
2023-06-27 18:21:12 +0300

    hope this work

--- server/src/utils/database.js
+++ server/src/utils/database.js
@@ -9,7 +9,7 @@ const sequelize = new Sequelize({
    database: process.env.MYSQLDATABASE,
    username: process.env.MYSQLUSER,
    password: process.env.MYSQLPASSWORD,
-   host: process.env.MYSQL_URL,
+   host: process.env.MYSQLHOST,
    port: process.env.MYSQLPORT,
    dialect: "mysql",
 });
